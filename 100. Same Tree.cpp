103d801d9cc52224a27e6d950af5c3a2f6ad116b
Zehua Yuan <37715823+NeoYY@users.noreply.github.com>
2020-05-30 12:45:57 -0400

    Create 100. Same Tree.cpp

---
+++ 100. Same Tree.cpp
@@ -0,0 +1,69 @@
+/* 100. Same Tree
+Easy
+
+1926
+
+56
+
+Add to List
+
+Share
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+
+Algorithm:
+1. if the node have different number of pointers, return false
+2. otherwise, recursive
+
+Below is the code
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if ( !p && !q ) return true;
+        else if ( !p || !q ) return false;
+        if ( p->val != q->val )return false;
+        return isSameTree( p->left, q->left ) && isSameTree( p->right, q->right );
+    }
+};
